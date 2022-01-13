c6b8365dce59ab53fbb73e24d95cefa271ab15d2
yoonui <yoony0612@ajou.ac.kr>
2022-01-13 17:10:40 +0900

    (22.01.13) 순수 JDBC

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,6 +35,15 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        // 멤버리스트를 모델에 담아서 화면에 넘김
+        return "members/memberList";
+    }
+
 
     // setter 주입
     /*
