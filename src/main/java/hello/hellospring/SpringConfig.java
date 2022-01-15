1b541de1209e9988cbec3185e58866a03c6ec7fd
yoonui <yoony0612@ajou.ac.kr>
2022-01-15 11:10:58 +0900

    (22.01.15) 다음으로

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
@@ -41,6 +42,13 @@ public class SpringConfig {
         // return new MemberService(memberRepository());
         return new MemberService(memberRepository);
     }
+
+    /*
+    @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }
+    */
 
     /*
     @Bean
